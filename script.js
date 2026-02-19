94a864a1020d6da25eee63a61b8cbd263c4e39e6
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-19 14:12:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-function getFormvalue() {
-    //Write your code here;
-	e.preventDefault();
-	let form  = document.forms["form1"];
+// function getFormvalue() {
+//     //Write your code here;
+// 	e.preventDefault();
+// 	let form  = document.forms["form1"];
 
-	let fn = form.fname.value.trim();
-		let ln = form.lname.value.trim();
+// 	let fn = form.fname.value.trim();
+// 	let ln = form.lname.value.trim();
 
-	let full_name = `${fn} ${ln}`.trim();
+// 	let full_name = `${fn} ${ln}`.trim();
 
-	if(full_name==""){
-	 alert("please enter your nmae");	
-	}
-	else{
-		alert(full_name);
-	}
-	return false;
+// 	if(full_name==""){
+// 	 alert("please enter your nmae");	
+// 	}
+// 	else{
+// 		alert(full_name);
+// 	}
+// 	return false;
 
 }
 
-// function getFormvalue() {
-//     let form = document.forms["form1"];
+function getFormvalue() {
+    let form = document.forms["form1"];
 
-//     let fn = form.fname.value.trim();
-//     let ln = form.lname.value.trim();
+    let fn = form.fname.value.trim();
+    let ln = form.lname.value.trim();
 
-//     let full_name = `${fn} ${ln}`.trim();
+    let full_name = `${fn} ${ln}`.trim();
 
-//     if (full_name === "") {
-//         alert("Please enter your name");
-//     } else {
-//         alert(full_name);
-//     }
+    if (full_name === "") {
+        alert("Please enter your name");
+    } else {
+        alert(full_name);
+    }
 
-//     return false; 
-// }
+    return false; 
+}
 
