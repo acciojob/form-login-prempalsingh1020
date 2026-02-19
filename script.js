7c23ed78a6ba8ac4f49e689fc9b602e014c535db
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-19 13:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(e) {
+    //Write your code here;
+	e.preventDefault();
+	let form  = document.forms["form1"];
+
+	let fn = form.fname.value.trim();
+		let ln = form.lname.value.trim();
+
+	let full_name = `${fn} ${ln}`.trim();
+
+	if(full_name==""){
+	 alert("please enter your nmae");	
+	}
+	else{
+		alert(full_name);
+	}
 
 }
